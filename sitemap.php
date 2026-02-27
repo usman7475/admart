1686526c8b54ea7b56084961abd24426e94f1a3c
Usman Mirza <usmanyounis7475@gmail.com>
2026-02-27 16:17:06 +0500

    Your message her3ddddde

--- sitemap.php
+++ sitemap.php
@@ -5,7 +5,7 @@ include 'admin/includes/config.php';
 
 
 
-// XML Header
+// XML H
 echo "<?xml version='1.0' encoding='UTF-8'?>\n";
 echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n";
 

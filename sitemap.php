0047676a40b56ed1b96fe29f81c19f4af9304d2d
Usman Mirza <usmanyounis7475@gmail.com>
2026-02-27 16:13:11 +0500

    Your message her3ddde

--- sitemap.php
+++ sitemap.php
@@ -9,7 +9,7 @@ include 'admin/includes/config.php';
 echo "<?xml version='1.0' encoding='UTF-8'?>\n";
 echo "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n";
 
-// Base URL (Update to your a
+// Base URL 
 $site_url="";
 
 // Static Pages
